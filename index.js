281c67df2aa4d498363d4c6fe65c07351c732365
oliviasabbah <olivia.sabbah06400@gmail.com>
2019-10-03 12:24:36 +0200

    njs1

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const path = require('path');
 const fichier = fs.readFileSync('./README.md', {encoding:'utf-8'}).toString()
 console.log(fichier)
 
+fs.readdirSync('./BRUCE/njs1/njs1/index.js', {encoding:'utf-8'} ) 
+  options = getOptions(options, {});
+  path = getValidatedPath(path);
+  array.forEach(readdirSync => {
+      console.log ("njs1" + readdirSync)
+      
+  });
+
+fs.readdirSync('./njs1' , {encoding: 'utf-8'})
+
 // écriture d'une chaine dans un fichier
 fs.writeFileSync('./lisezmoi.md', 'readme!');
 fs.appendFileSync('./lisezmoi.md', ' lisez moi!');
-fs.appendFileSync('./lisez.md', '\n lisez moi!');
+fs.appendFileSync('./lisez.md', ' lisez moi!');
 // créer un dossier s'il n'existe pas
 var dir = 'public';
 if (fs.existsSync(dir)) {
